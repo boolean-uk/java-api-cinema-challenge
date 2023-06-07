6e40894bdc188c876055663bf759d27674215d7c
Dennis <dennisvoutos@gmail.com>
2023-06-07 15:27:28 +0300

    Customer controller done

--- src/main/java/com/booleanuk/api/cinema/controller/CustomerController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/CustomerController.java
@@ -3,9 +3,10 @@ package com.booleanuk.api.cinema.controller;
 import com.booleanuk.api.cinema.model.Customer;
 import com.booleanuk.api.cinema.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -17,5 +18,40 @@ public class CustomerController {
     @GetMapping
     public List<Customer> getAll(){
         return this.repository.findAll();
+    }
+    //not necessary but will come in handy later
+    @GetMapping("/{id}")
+    public Customer getCustomer(@PathVariable int id){
+        return this.repository.findById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Customer with given id not found"));
+    }
+    public record CustomerRequest(String name, String email,String phone){}
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody CustomerRequest customer){
+        Customer createdCustomer = new Customer(customer.name,customer.email, customer.phone);
+        return new ResponseEntity<>(createdCustomer,HttpStatus.OK);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable int id, @RequestBody CustomerRequest customer){
+        Customer updatedCustomer = this.getCustomer(id);
+        if(customer.name != null && !customer.name.isEmpty()){
+            updatedCustomer.setName(customer.name);
+            updatedCustomer.setUpdatedAt();
+        }
+        if(customer.phone != null && !customer.phone.isEmpty()){
+            updatedCustomer.setPhone(customer.phone);
+            updatedCustomer.setUpdatedAt();
+        }
+        if(customer.email != null && !customer.email.isEmpty()){
+            updatedCustomer.setEmail(customer.email);
+            updatedCustomer.setUpdatedAt();
+        }
+        return new ResponseEntity<>(updatedCustomer,HttpStatus.OK);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable int id){
+        Customer toDelete = getCustomer(id);
+        this.repository.delete(toDelete);
+        return new ResponseEntity<>(toDelete,HttpStatus.OK);
     }
 }
