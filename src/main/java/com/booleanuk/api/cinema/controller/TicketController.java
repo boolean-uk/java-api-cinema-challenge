72f770443994ed8ca54042226eb86a54393ce1ac
Dennis <dennisvoutos@gmail.com>
2023-06-08 15:59:38 +0300

    added extra things not required

---
+++ src/main/java/com/booleanuk/api/cinema/controller/TicketController.java
@@ -0,0 +1,25 @@
+package com.booleanuk.api.cinema.controller;
+
+import com.booleanuk.api.cinema.model.Screening;
+import com.booleanuk.api.cinema.model.Ticket;
+import com.booleanuk.api.cinema.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//this is extra, in case i want to see all available tickets
+@RestController
+@RequestMapping("tickets")
+public class TicketController {
+    @Autowired
+    private TicketRepository repository;
+
+    //in case i want to see all tickets
+    @GetMapping
+    public List<Ticket> getAll(){
+        return this.repository.findAll();
+    }
+}
