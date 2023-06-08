2588834a9dcd81bd3ee49949b60843fe933ebbc0
brightWin <konstantinos.vroustouris@gmail.com>
2023-06-08 17:44:46 +0300

    added @JsonSerializer and @JsonDeserializer in Screening::startAt

--- src/main/java/com/booleanuk/api/cinema/GenericResponse.java
+++ src/main/java/com/booleanuk/api/cinema/GenericResponse.java
@@ -1,5 +1,6 @@
 package com.booleanuk.api.cinema;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,17 @@ public class GenericResponse<T> {
     private static String SUCCESS_STATUS = "success";
     private String status;
     private T data;
+    @JsonIgnore
+    private Class<T> type;
 
+    public GenericResponse(Class<T> type){
+        this.type = type;
+    }
+
+    public GenericResponse(String status, T data){
+        this.status = status;
+        this.data = data;
+    }
 
     public GenericResponse<T> from(T data){
         return new GenericResponse<>(
---
+++ src/main/java/com/booleanuk/api/cinema/formatter/CustomDateSerializer.java
@@ -0,0 +1,26 @@
+package com.booleanuk.api.cinema.formatter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.cglib.core.Local;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class CustomDateSerializer extends JsonSerializer<Date> {
+    private static final SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+    @Override
+    public void serialize(Date value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        String dateString = FORMATTER.format(value);
+        gen.writeString(dateString);
+    }
+}
