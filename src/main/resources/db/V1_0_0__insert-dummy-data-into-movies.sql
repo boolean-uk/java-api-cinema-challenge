a913fa8e6b85a297e60faec3af033e444e074eb8
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-06-10 20:26:58 +0300

    Integrated Flyway in project. Added dummy data

--- src/main/resources/db/V1_0_0__insert-dummy-data-into-movies.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO movie (id, title, rating, description, runtime_mins, created_at, updated_at)
-VALUES
-    (1, 'The Shawshank Redemption', '9.3', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 142, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (2, 'The Godfather', '9.2', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', 175, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (3, 'Pulp Fiction', '8.9', 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.', 154, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (4, 'The Dark Knight', '9.0', 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.', 152, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (5, 'Fight Club', '8.8', 'An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.', 139, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
