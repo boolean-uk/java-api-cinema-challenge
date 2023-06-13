ded371e77d7804cf011c61c00b4dc3210dce3251
Konstantinos Vroustouris <102743301+brightdean@users.noreply.github.com>
2023-06-14 01:08:15 +0300

    Update Main.java

--- src/main/java/com/booleanuk/api/cinema/Main.java
+++ src/main/java/com/booleanuk/api/cinema/Main.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-    //TODO fix startsAt Date not being set correctly
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
