1685cc05447ec3975282bc3bc6c79f38b4b1b5a6
iliask796 <iliaskal7@gmail.com>
2023-10-09 16:46:49 +0300

    DataResponse: Customer

---
+++ src/main/java/com/booleanuk/api/cinema/utility/CustomerListResponse.java
@@ -0,0 +1,8 @@
+package com.booleanuk.api.cinema.utility;
+
+import com.booleanuk.api.cinema.model.Customer;
+
+import java.util.List;
+
+public class CustomerListResponse extends DataResponse<List<Customer>> {
+}
---
+++ src/main/java/com/booleanuk/api/cinema/utility/CustomerResponse.java
@@ -0,0 +1,6 @@
+package com.booleanuk.api.cinema.utility;
+
+import com.booleanuk.api.cinema.model.Customer;
+
+public class CustomerResponse extends DataResponse<Customer>{
+}
