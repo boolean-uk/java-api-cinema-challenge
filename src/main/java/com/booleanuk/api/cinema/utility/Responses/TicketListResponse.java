e64552717ef7776a69db48f607fc23dac583c624
iliask796 <iliaskal7@gmail.com>
2023-10-10 16:10:17 +0300

    DataResponse: Ticket Response Added

---
+++ src/main/java/com/booleanuk/api/cinema/utility/Responses/TicketListResponse.java
@@ -0,0 +1,9 @@
+package com.booleanuk.api.cinema.utility.Responses;
+
+import com.booleanuk.api.cinema.model.Ticket;
+import com.booleanuk.api.cinema.utility.DataResponse;
+
+import java.util.List;
+
+public class TicketListResponse extends DataResponse<List<Ticket>> {
+}
---
+++ src/main/java/com/booleanuk/api/cinema/utility/Responses/TicketResponse.java
@@ -0,0 +1,7 @@
+package com.booleanuk.api.cinema.utility.Responses;
+
+import com.booleanuk.api.cinema.model.Ticket;
+import com.booleanuk.api.cinema.utility.DataResponse;
+
+public class TicketResponse extends DataResponse<Ticket> {
+}
