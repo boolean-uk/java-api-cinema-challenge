290f47619fbd996d041550783541cd5ac673f5e5
Irene <irene.maniou@hotmail.com>
2023-10-11 10:43:21 +0300

    Irene Maniou updated -> Ticket updated

--- src/main/java/com/booleanuk/api/cinema/movie/MovieRepository.java
+++ src/main/java/com/booleanuk/api/cinema/movie/MovieRepository.java
@@ -1,7 +1,10 @@
 package com.booleanuk.api.cinema.movie;
 
 import com.booleanuk.api.cinema.customer.Customer;
+import com.booleanuk.api.cinema.screening.Screening;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
--- src/main/java/com/booleanuk/api/cinema/ticket/TicketRepository.java
+++ src/main/java/com/booleanuk/api/cinema/ticket/TicketRepository.java
@@ -1,6 +1,10 @@
 package com.booleanuk.api.cinema.ticket;
 
+import com.booleanuk.api.cinema.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    List<Ticket> findByCustomerIdAndScreeningId(int customerId, int screeningId);
 }
