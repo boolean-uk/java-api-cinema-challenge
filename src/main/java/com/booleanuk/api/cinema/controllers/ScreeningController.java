414a98d15a24eeea742874a092ca49f04d0e15a9
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-02-09 08:51:52 +0100

    Added all imports

--- src/main/java/com/booleanuk/api/cinema/controllers/ScreeningController.java
+++ src/main/java/com/booleanuk/api/cinema/controllers/ScreeningController.java
@@ -1,4 +1,10 @@
 package com.booleanuk.api.cinema.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("screenings")
 public class ScreeningController {
+
 }
