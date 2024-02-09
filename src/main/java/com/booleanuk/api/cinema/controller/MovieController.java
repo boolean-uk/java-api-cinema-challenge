ad7ad20fda1f1a92c7cb9f76a3dfabc7aaa3b747
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-09 10:40:21 +0100

    Create getAll methods

--- src/main/java/com/booleanuk/api/cinema/controller/MovieController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/MovieController.java
@@ -1,13 +1,22 @@
 package com.booleanuk.api.cinema.controller;
 
+import com.booleanuk.api.cinema.dto.MovieDto;
 import com.booleanuk.api.cinema.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("movies")
 public class MovieController {
     @Autowired
     MovieRepository repository;
+
+    @GetMapping
+    public List<MovieDto> getAllMovies() {
+        return this.repository.findAllProjectedBy();
+    }
 }
--- src/main/java/com/booleanuk/api/cinema/controller/ScreeningController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/ScreeningController.java
@@ -1,13 +1,23 @@
 package com.booleanuk.api.cinema.controller;
 
+import com.booleanuk.api.cinema.dto.ScreeningDto;
 import com.booleanuk.api.cinema.repository.ScreeningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("screenings")
+@RequestMapping("movies/{movId}/screenings")
 public class ScreeningController {
     @Autowired
     ScreeningRepository repository;
+
+    @GetMapping
+    public List<ScreeningDto> getAllScreeningOffMovie(@PathVariable int movId) {
+        return this.repository.findByMovieId(movId);
+    }
 }
