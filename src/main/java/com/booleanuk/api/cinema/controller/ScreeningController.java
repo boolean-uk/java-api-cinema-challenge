2577865b6432f84d3401e81f781727fd2102bfb2
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-09 12:50:28 +0100

    Create screening create endpoint

--- src/main/java/com/booleanuk/api/cinema/controller/ScreeningController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/ScreeningController.java
@@ -1,13 +1,19 @@
 package com.booleanuk.api.cinema.controller;
 
+import com.booleanuk.api.cinema.dto.CustomerDto;
 import com.booleanuk.api.cinema.dto.ScreeningDto;
+import com.booleanuk.api.cinema.model.Customer;
+import com.booleanuk.api.cinema.model.Screening;
+import com.booleanuk.api.cinema.repository.MovieRepository;
 import com.booleanuk.api.cinema.repository.ScreeningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,8 +22,27 @@ public class ScreeningController {
     @Autowired
     ScreeningRepository repository;
 
+    @Autowired
+    MovieRepository movieRepository;
+
     @GetMapping
     public List<ScreeningDto> getAllScreeningOffMovie(@PathVariable int movId) {
         return this.repository.findByMovieId(movId);
+    }
+
+    @PostMapping
+    public ResponseEntity<ScreeningDto> createScreening(@PathVariable int movId, @RequestBody Screening screening) {
+        if (screening.getScreenNumber() < 0 || screening.getCapacity() < 0 || screening.getStartsAt() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request");
+        }
+        screening.setMovie(this.movieRepository.findById(movId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found")));
+        Screening createdScreening = this.repository.save(screening);
+        createdScreening.setTickets(new ArrayList<>());
+        return new ResponseEntity<>(this.translateToDto(createdScreening), HttpStatus.CREATED);
+    }
+
+    public ScreeningDto translateToDto(Screening screening) {
+        return new ScreeningDto(screening.getId(), screening.getScreenNumber(), screening.getCapacity(), screening.getStartsAt(), screening.getCreatedAt(), screening.getUpdatedAt());
     }
 }
