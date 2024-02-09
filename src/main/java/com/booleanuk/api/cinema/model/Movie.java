8a03139d17b3469ced510d14a43e78a3f6b5ae28
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-09 09:45:47 +0100

    Create Movie Class

---
+++ src/main/java/com/booleanuk/api/cinema/model/Movie.java
@@ -0,0 +1,53 @@
+package com.booleanuk.api.cinema.model;
+
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String rating;
+
+    @Column
+    private String description;
+
+    @Column
+    private int runtimeMins;
+
+    @OneToMany(mappedBy = "movie")
+    @JsonIncludeProperties(value = {})
+    private List<Screening> screenings;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public Movie(String title, String rating, String description, int runtimeMins, List<Screening> screenings) {
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+        this.runtimeMins = runtimeMins;
+        this.screenings = screenings;
+    }
+}
