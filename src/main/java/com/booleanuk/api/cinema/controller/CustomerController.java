d8b479bd88165f8dfa07723fbdb0716b0d11f947
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-09 11:09:40 +0100

    Create customer create endpoint

--- src/main/java/com/booleanuk/api/cinema/controller/CustomerController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/CustomerController.java
@@ -1,12 +1,15 @@
 package com.booleanuk.api.cinema.controller;
 
 import com.booleanuk.api.cinema.dto.CustomerDto;
+import com.booleanuk.api.cinema.model.Customer;
 import com.booleanuk.api.cinema.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,5 +21,19 @@ public class CustomerController {
     @GetMapping
     public List<CustomerDto> getAllCustomers() {
         return this.repository.findAllProjectedBy();
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerDto> createCustomer(@RequestBody Customer customer) {
+        if (customer.getEmail() == null || customer.getName() == null || customer.getPhone() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request");
+        }
+        Customer createdCustomer = this.repository.save(customer);
+        createdCustomer.setTickets(new ArrayList<>());
+        return new ResponseEntity<>(this.translateToDto(createdCustomer), HttpStatus.CREATED);
+    }
+
+    public CustomerDto translateToDto(Customer customer) {
+        return new CustomerDto(customer.getId(), customer.getName(), customer.getEmail(), customer.getPhone(), customer.getCreatedAt(), customer.getUpdatedAt());
     }
 }
