23c312e5bc45bccd7a973177d39ac7b3b754947f
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-09 15:09:19 +0100

    Add new API response form to Screening

--- src/main/java/com/booleanuk/api/cinema/controller/ScreeningController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/ScreeningController.java
@@ -1,9 +1,12 @@
 package com.booleanuk.api.cinema.controller;
 
 import com.booleanuk.api.cinema.dto.ScreeningDto;
+import com.booleanuk.api.cinema.model.Movie;
 import com.booleanuk.api.cinema.model.Screening;
 import com.booleanuk.api.cinema.repository.MovieRepository;
 import com.booleanuk.api.cinema.repository.ScreeningRepository;
+import com.booleanuk.api.cinema.response.ApiException;
+import com.booleanuk.api.cinema.response.ApiSuccessResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,20 +26,25 @@ public class ScreeningController {
     MovieRepository movieRepository;
 
     @GetMapping
-    public List<ScreeningDto> getAllScreeningOffMovie(@PathVariable int movId) {
-        return this.repository.findByMovieId(movId);
+    public ResponseEntity<ApiSuccessResponse<List<ScreeningDto>>> getAllScreeningOffMovie(@PathVariable int movId) {
+        List<Screening> screenings = this.movieRepository.findById(movId)
+                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "not found")).getScreenings();
+        if (screenings.isEmpty()) {
+            throw new ApiException(HttpStatus.NOT_FOUND, "No screenings of this movie");
+        }
+        return ResponseEntity.ok(new ApiSuccessResponse<>(this.repository.findByMovieId(movId)));
     }
 
     @PostMapping
-    public ResponseEntity<ScreeningDto> createScreening(@PathVariable int movId, @RequestBody Screening screening) {
-        if (screening.getScreenNumber() < 0 || screening.getCapacity() < 0 || screening.getStartsAt() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request");
+    public ResponseEntity<ApiSuccessResponse<ScreeningDto>> createScreening(@PathVariable int movId, @RequestBody Screening screening) {
+        if (screening.getScreenNumber() <= 0 || screening.getCapacity() <= 0 || screening.getStartsAt() == null) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "bad request");
         }
         screening.setMovie(this.movieRepository.findById(movId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found")));
+                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "not found")));
         Screening createdScreening = this.repository.save(screening);
         createdScreening.setTickets(new ArrayList<>());
-        return new ResponseEntity<>(this.translateToDto(createdScreening), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ApiSuccessResponse<>(this.translateToDto(createdScreening)), HttpStatus.CREATED);
     }
 
     public ScreeningDto translateToDto(Screening screening) {
