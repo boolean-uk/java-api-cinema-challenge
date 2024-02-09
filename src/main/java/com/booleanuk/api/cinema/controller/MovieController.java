dd3a93f365266f7469980920dfe81e6eba2970a7
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-09 11:14:14 +0100

    Create movie create endpoint

--- src/main/java/com/booleanuk/api/cinema/controller/MovieController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/MovieController.java
@@ -1,12 +1,15 @@
 package com.booleanuk.api.cinema.controller;
 
 import com.booleanuk.api.cinema.dto.MovieDto;
+import com.booleanuk.api.cinema.model.Movie;
 import com.booleanuk.api.cinema.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,5 +21,19 @@ public class MovieController {
     @GetMapping
     public List<MovieDto> getAllMovies() {
         return this.repository.findAllProjectedBy();
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieDto> createMovie(@RequestBody Movie movie) {
+        if (movie.getTitle() == null || movie.getRating() == null || movie.getDescription() == null || movie.getRuntimeMins() < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request");
+        }
+        Movie createdMovie = this.repository.save(movie);
+        createdMovie.setScreenings(new ArrayList<>());
+        return new ResponseEntity<>(this.translateToDto(createdMovie), HttpStatus.CREATED);
+    }
+
+    public MovieDto translateToDto(Movie movie) {
+        return new MovieDto(movie.getId(), movie.getTitle(), movie.getRating(), movie.getDescription(), movie.getRuntimeMins(), movie.getCreatedAt(), movie.getUpdatedAt());
     }
 }
