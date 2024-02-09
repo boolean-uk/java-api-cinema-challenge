1dfc82f6899d5840779162fd25d962008da74a30
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-09 10:17:40 +0100

    Set up projection find methods in repositories

--- src/main/java/com/booleanuk/api/cinema/repository/CustomerRepository.java
+++ src/main/java/com/booleanuk/api/cinema/repository/CustomerRepository.java
@@ -1,7 +1,12 @@
 package com.booleanuk.api.cinema.repository;
 
+import com.booleanuk.api.cinema.dto.CustomerDto;
 import com.booleanuk.api.cinema.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    List<CustomerDto> findAllProjectedBy();
+    CustomerDto findByIdProjectedBy(int id);
 }
--- src/main/java/com/booleanuk/api/cinema/repository/ScreeningRepository.java
+++ src/main/java/com/booleanuk/api/cinema/repository/ScreeningRepository.java
@@ -1,7 +1,12 @@
 package com.booleanuk.api.cinema.repository;
 
+import com.booleanuk.api.cinema.dto.ScreeningDto;
 import com.booleanuk.api.cinema.model.Screening;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScreeningRepository extends JpaRepository<Screening, Integer> {
+    List<ScreeningDto> findAllProjectedBy();
+    ScreeningDto findByIdProjectedBy(int id);
 }
