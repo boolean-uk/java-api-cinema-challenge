12b656c88d7d12f80cfe4dbd659f3ed46c5bcb32
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-09 15:19:52 +0100

    Create Ticket get screening for customer endpoint

--- src/main/java/com/booleanuk/api/cinema/controller/TicketController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/TicketController.java
@@ -1,13 +1,31 @@
 package com.booleanuk.api.cinema.controller;
 
+import com.booleanuk.api.cinema.dto.TicketDto;
 import com.booleanuk.api.cinema.repository.TicketRepository;
+import com.booleanuk.api.cinema.response.ApiException;
+import com.booleanuk.api.cinema.response.ApiSuccessResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("tickets")
+@RequestMapping("customers/{customerId}/screenings/{screeningId}")
 public class TicketController {
     @Autowired
     TicketRepository repository;
+
+    @GetMapping
+    public ResponseEntity<ApiSuccessResponse<List<TicketDto>>> getScreeningsForCustomer(@PathVariable int customerId, @PathVariable int screeningId) {
+        List<TicketDto> tickets = this.repository.findByCustomerIdAndScreeningId(customerId, screeningId);
+        if (tickets.isEmpty()) {
+            throw new ApiException(HttpStatus.NOT_FOUND, "not found");
+        }
+        return ResponseEntity.ok(new ApiSuccessResponse<>(tickets));
+    }
 }
--- src/main/java/com/booleanuk/api/cinema/repository/TicketRepository.java
+++ src/main/java/com/booleanuk/api/cinema/repository/TicketRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
-    List<TicketDto> findAllProjectedBy();
+    List<TicketDto> findByCustomerIdAndScreeningId(int customerId, int screeningId);
     //TicketDto findByIdProjectedBy(int id);
 }
