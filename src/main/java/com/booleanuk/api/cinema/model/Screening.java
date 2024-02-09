75549949349caabe64751fb8b0e7953ddf72b591
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-02-09 09:08:17 +0100

    Add Spring and setup classes

---
+++ src/main/java/com/booleanuk/api/cinema/model/Screening.java
@@ -0,0 +1,17 @@
+package com.booleanuk.api.cinema.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "screenings")
+public class Screening
+{
+}
---
+++ src/main/java/com/booleanuk/api/cinema/model/Ticket.java
@@ -0,0 +1,15 @@
+package com.booleanuk.api.cinema.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+}
