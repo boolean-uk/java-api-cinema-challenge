d67918f90763cf2eeeb94b3dc628f43920938399
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-02-12 10:04:34 +0100

    Save screenings when creating movie and delete related screenings when deleting movie.

--- src/main/java/com/booleanuk/api/cinema/controller/MovieController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.booleanuk.api.cinema.controller;
 
 import com.booleanuk.api.cinema.model.Movie;
+import com.booleanuk.api.cinema.model.Screening;
 import com.booleanuk.api.cinema.repository.MovieRepository;
 import com.booleanuk.api.cinema.repository.ScreeningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,10 +26,22 @@ public class MovieController {
     @PostMapping
     public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
         this.checkHasRequiredFields(movie);
-        // TODO: If screenings included in request add them to screenings table with correct movie relationship
         movie.setCreatedAt(ZonedDateTime.now());
         movie.setUpdatedAt(ZonedDateTime.now());
-        return new ResponseEntity<>(this.movieRepository.save(movie), HttpStatus.CREATED);
+        Movie saved = this.movieRepository.save(movie);
+        if(movie.getScreenings() != null) {
+            for (Screening screening : movie.getScreenings()) {
+                checkScreeningHasRequiredFields(screening);
+                screening.setMovie(saved);
+                screening.setCreatedAt(ZonedDateTime.now());
+                screening.setUpdatedAt(ZonedDateTime.now());
+                this.screeningRepository.save(screening);
+            }
+        }
+        else {
+            movie.setScreenings(new ArrayList<>());
+        }
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
     @GetMapping
@@ -50,8 +64,10 @@ public class MovieController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Movie> deleteMovie(@PathVariable int id) {
-        // TODO: On deletion, all connected screenings should be deleted.
         Movie movieToDelete = this.findMovieById(id);
+        for (Screening screening : movieToDelete.getScreenings()) {
+            screeningRepository.delete(screening);
+        }
         this.movieRepository.delete(movieToDelete);
         return ResponseEntity.ok(movieToDelete);
     }
@@ -66,6 +82,13 @@ public class MovieController {
     private void checkHasRequiredFields(Movie movie) {
         if (movie.getTitle() == null || movie.getRating() == null || movie.getDescription() == null || movie.getRuntimeMins() <= 0) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not create a new movie, please check all required fields are correct.");
+        }
+    }
+
+    // Method to check if all required fields are contained in screening
+    private void checkScreeningHasRequiredFields(Screening screening) {
+        if (screening.getScreenNumber() <= 0 || screening.getCapacity() <= 0 || screening.getStartsAt() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not create a screening for the specified movie, please check all required fields are correct.");
         }
     }
 }
