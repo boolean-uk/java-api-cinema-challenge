0ca4ef563b9f5ada8108dc7490607391c3390038
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-10 16:19:26 +0200

    Implemented movie model

---
+++ src/main/java/com/booleanuk/api/cinema/model/Movie.java
@@ -0,0 +1,99 @@
+package com.booleanuk.api.cinema.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String rating;
+
+    @Column
+    private String description;
+
+    @Column
+    private int runtimeMins;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    public Movie() {
+
+    }
+
+    public Movie(String title, String rating, String description, int runtimeMins) {
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+        this.runtimeMins = runtimeMins;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRuntimeMins() {
+        return runtimeMins;
+    }
+
+    public void setRuntimeMins(int runtimeMins) {
+        this.runtimeMins = runtimeMins;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
