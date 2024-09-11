ae5bb78c879f52f114a68069d3f2bc74f3ccc8cb
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-11 09:12:45 +0200

    created customer

---
+++ src/main/java/com/booleanuk/api/cinema/models/Customer.java
@@ -0,0 +1,35 @@
+package com.booleanuk.api.cinema.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    public Customer(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Customer(int id) {
+        this.id = id;
+    }
+}
