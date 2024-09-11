1bccb77de285cc3c98c543ccfa7fa31f991b998a
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-11 09:17:50 +0200

    created screening

---
+++ src/main/java/com/booleanuk/api/cinema/models/Screening.java
@@ -0,0 +1,32 @@
+package com.booleanuk.api.cinema.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "screenings")
+public class Screening {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int screenNumber;
+    private int capacity;
+    private String startsAt;
+
+    public Screening(int screenNumber, int capacity, String startsAt) {
+        this.screenNumber = screenNumber;
+        this.capacity = capacity;
+        this.startsAt = startsAt;
+    }
+
+    public Screening(int id) {
+        this.id = id;
+    }
+}
