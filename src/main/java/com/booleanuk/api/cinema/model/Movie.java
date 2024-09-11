4acde0713eeaf9fc93beef9eda66e00c65b9fd94
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-11 10:32:42 +0200

    .

--- src/main/java/com/booleanuk/api/cinema/model/Movie.java
+++ src/main/java/com/booleanuk/api/cinema/model/Movie.java
@@ -1,9 +1,107 @@
 package com.booleanuk.api.cinema.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "movies")
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "rating")
+    private String rating;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "runtimeMin")
+    private Integer runtimeMin;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Movie(String title, String rating, String description, Integer runtimeMin) {
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+        this.runtimeMin = runtimeMin;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Movie(String title, String rating, String description, Integer runtimeMin, LocalDateTime time) {
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+        this.runtimeMin = runtimeMin;
+        this.updatedAt = time;
+    }
+
+    public Movie() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getRuntimeMin() {
+        return runtimeMin;
+    }
+
+    public void setRuntimeMin(Integer runtimeMin) {
+        this.runtimeMin = runtimeMin;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
