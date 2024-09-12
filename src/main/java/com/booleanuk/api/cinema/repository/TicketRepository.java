34741dd74226783f4806d2f016969f80e429d670
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-12 11:13:33 +0200

    Added getting a list of tickets for a given customer and screening in the ticket repository

--- src/main/java/com/booleanuk/api/cinema/repository/TicketRepository.java
+++ src/main/java/com/booleanuk/api/cinema/repository/TicketRepository.java
@@ -3,5 +3,8 @@ package com.booleanuk.api.cinema.repository;
 import com.booleanuk.api.cinema.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    public List<Ticket> findAllByCustomerIdAndScreeningId(int customerId, int screeningId);
 }
