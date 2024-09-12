a89a68b0573fd8ba3f97c74cd218de999f769dc0
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-12 11:14:59 +0200

    Implemented ticket controller. Extension done

--- src/main/java/com/booleanuk/api/cinema/controller/TicketController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/TicketController.java
@@ -5,9 +5,10 @@ import com.booleanuk.api.cinema.model.Movie;
 import com.booleanuk.api.cinema.model.Screening;
 import com.booleanuk.api.cinema.model.Ticket;
 import com.booleanuk.api.cinema.repository.CustomerRepository;
-import com.booleanuk.api.cinema.repository.MovieRepository;
 import com.booleanuk.api.cinema.repository.ScreeningRepository;
 import com.booleanuk.api.cinema.repository.TicketRepository;
+import com.booleanuk.api.cinema.responses.ErrorResponse;
+import com.booleanuk.api.cinema.responses.Response;
 import com.booleanuk.api.cinema.responses.TicketListResponse;
 import com.booleanuk.api.cinema.responses.TicketResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -32,33 +34,58 @@ public class TicketController {
     private ScreeningRepository screenings;
 
     @PostMapping
-    public ResponseEntity<Ticket> create(@PathVariable(name = "customerId") int customerId,
-                                         @PathVariable(name = "screeningId") int screeningId,
-                                         @RequestBody Ticket ticket) {
+    public ResponseEntity<Response<?>> create(@PathVariable(name = "customerId") int customerId,
+                                              @PathVariable(name = "screeningId") int screeningId,
+                                              @RequestBody Ticket ticket) {
         Customer customer = this.customers.findById(customerId)
-                .orElseThrow(
-                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "not found")
+                .orElse(
+                        null
                 );
 
-        Screening screening = this.screenings.findById(customerId)
-                .orElseThrow(
-                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "not found")
+        Screening screening = this.screenings.findById(screeningId)
+                .orElse(
+                        null
                 );
+
+        if (customer == null || screening == null) {
+            ErrorResponse error = new ErrorResponse("not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+
+        ticket.setCreatedAt(LocalDateTime.now());
+        ticket.setUpdatedAt(LocalDateTime.now());
+        ticket.setCustomer(customer);
+        ticket.setScreening(screening);
 
         TicketResponse ticketResponse = new TicketResponse();
         ticketResponse.set(this.tickets.save(ticket));
 
-        return null;
+        return new ResponseEntity<>(ticketResponse, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<TicketListResponse> getAll() {
+    public ResponseEntity<Response<?>> getAll(@PathVariable(name = "customerId") int customerId,
+                                              @PathVariable(name = "screeningId") int screeningId) {
+        Customer customer = this.customers.findById(customerId)
+                .orElse(
+                        null
+                );
 
+        Screening screening = this.screenings.findById(screeningId)
+                .orElse(
+                        null
+                );
+
+        if (customer == null || screening == null) {
+            ErrorResponse error = new ErrorResponse("not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+
+        List<Ticket> toReturn = this.tickets.findAllByCustomerIdAndScreeningId(customerId, screeningId);
         TicketListResponse ticketListResponse = new TicketListResponse();
-        this.tickets.findAllById()
-        ticketListResponse.set(this.tickets.findById(1));
+        ticketListResponse.set(toReturn);
 
-        return null;
+        return new ResponseEntity<>(ticketListResponse, HttpStatus.OK);
     }
 
 
