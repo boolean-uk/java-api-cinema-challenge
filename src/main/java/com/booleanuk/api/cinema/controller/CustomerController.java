ac73208430ef22852708b6ad9aad1309b3fcb5be
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-12 09:19:30 +0200

    Refractor customer controller for reponse objects

--- src/main/java/com/booleanuk/api/cinema/controller/CustomerController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/CustomerController.java
@@ -5,13 +5,14 @@ import com.booleanuk.api.cinema.repository.CustomerRepository;
 
 import java.time.LocalDateTime;
 
+import com.booleanuk.api.cinema.responses.CustomerListResponse;
+import com.booleanuk.api.cinema.responses.CustomerResponse;
+import com.booleanuk.api.cinema.responses.ErrorResponse;
+import com.booleanuk.api.cinema.responses.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("customers")
@@ -28,43 +29,75 @@ public class CustomerController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Customer>> getAll() {
-        return new ResponseEntity<>(this.customers.findAll(), HttpStatus.OK);
+    public ResponseEntity<CustomerListResponse> getAll() {
+        CustomerListResponse customerListResponse = new CustomerListResponse();
+        customerListResponse.set(this.customers.findAll());
+
+        return new ResponseEntity<>(customerListResponse, HttpStatus.OK);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Customer> getOne(@PathVariable int id) {
+    public ResponseEntity<Response<?>> getOne(@PathVariable int id) {
         Customer toReturn = this.customers.findById(id)
-                .orElseThrow(
-                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+                .orElse(
+                        null
                 );
-        return new ResponseEntity<>(toReturn, HttpStatus.OK);
+
+        if (toReturn == null) {
+            return new ResponseEntity<>(new ErrorResponse("not found"), HttpStatus.NOT_FOUND);
+        }
+
+        CustomerResponse customerResponse = new CustomerResponse();
+        customerResponse.set(toReturn);
+
+        return new ResponseEntity<>(customerResponse, HttpStatus.OK);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Customer> delete(@PathVariable int id) {
+    public ResponseEntity<Response<?>> delete(@PathVariable int id) {
         Customer toDelete = this.customers.findById(id)
-                .orElseThrow(
-                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+                .orElse(
+                        null
                 );
+
+
+        if (toDelete == null) {
+            ErrorResponse error = new ErrorResponse("not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+
         this.customers.delete(toDelete);
-        return new ResponseEntity<>(toDelete, HttpStatus.OK);
+        CustomerResponse customerResponse = new CustomerResponse();
+        customerResponse.set(toDelete);
+        return new ResponseEntity<>(customerResponse, HttpStatus.OK);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Customer> update(@PathVariable int id, @RequestBody Customer newData) {
+    public ResponseEntity<Response<?>> update(@PathVariable int id, @RequestBody Customer newData) {
         Customer toUpdate = this.customers.findById(id)
-                .orElseThrow(
-                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+                .orElse(
+                        null
                 );
+
+        // 404 customer not found
+        if (toUpdate == null) {
+            return new ResponseEntity<>(new ErrorResponse("not found"), HttpStatus.NOT_FOUND);
+        }
+
+        // 400 one or more fields are not correct
+        if (newData.getEmail() == null || newData.getName() == null || newData.getPhone() == null) {
+            return new ResponseEntity<>(new ErrorResponse("bad request"), HttpStatus.BAD_REQUEST);
+        }
 
         toUpdate.setName(newData.getName());
         toUpdate.setEmail(newData.getEmail());
         toUpdate.setPhone(newData.getPhone());
-
         toUpdate.setUpdatedAt(LocalDateTime.now());
 
-        return new ResponseEntity<>(this.customers.save(toUpdate), HttpStatus.CREATED);
+        CustomerResponse customerResponse = new CustomerResponse();
+        customerResponse.set(this.customers.save(toUpdate));
+
+        return new ResponseEntity<>(customerResponse, HttpStatus.CREATED);
     }
 
 }
