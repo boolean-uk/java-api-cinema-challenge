fb8ef8ad0d52dad9b80810ff24ceccb518d1f85d
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-28 09:18:48 +0100

    Extend repositories

--- src/main/java/com/booleanuk/api/cinema/repository/CustomerRepository.java
+++ src/main/java/com/booleanuk/api/cinema/repository/CustomerRepository.java
@@ -1,4 +1,7 @@
 package com.booleanuk.api.cinema.repository;
 
-public interface CustomerRepository {
+import com.booleanuk.api.cinema.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
--- src/main/java/com/booleanuk/api/cinema/repository/MovieRepository.java
+++ src/main/java/com/booleanuk/api/cinema/repository/MovieRepository.java
@@ -1,4 +1,7 @@
 package com.booleanuk.api.cinema.repository;
 
-public interface MovieRepository {
+import com.booleanuk.api.cinema.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
