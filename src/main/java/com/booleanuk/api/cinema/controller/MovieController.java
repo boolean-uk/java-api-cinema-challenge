8a90adc7b6e7c6feaa8f9477aae67cabfa68fa4f
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-28 10:49:48 +0100

    Refactor some screening endpoints to MovieController

--- src/main/java/com/booleanuk/api/cinema/controller/MovieController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/MovieController.java
@@ -1,24 +1,26 @@
 package com.booleanuk.api.cinema.controller;
 
 import com.booleanuk.api.cinema.model.Movie;
+import com.booleanuk.api.cinema.model.Screening;
 import com.booleanuk.api.cinema.repository.MovieRepository;
+import com.booleanuk.api.cinema.repository.ScreeningRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
     private MovieRepository movieRepository;
+    private ScreeningRepository screeningRepository;
 
-    public MovieController(MovieRepository movieRepository) {
+    public MovieController(MovieRepository movieRepository, ScreeningRepository screeningRepository) {
         this.movieRepository = movieRepository;
+        this.screeningRepository = screeningRepository;
     }
 
     @GetMapping("/{id}")
@@ -31,5 +33,28 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAll() {
         return ResponseEntity.ok(movieRepository.findAll());
+    }
+
+    @PostMapping("{id}/screenings")
+    public ResponseEntity<Screening> create(@PathVariable int movieId, @RequestBody Screening screening) {
+        Movie movie = movieRepository.findById(movieId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No movie with that ID was found.")
+        );
+        screening.setMovie(movie);
+        screening.setCreatedAt(LocalDateTime.now());
+        screening.setUpdatedAt(LocalDateTime.now());
+        return new ResponseEntity<>(screeningRepository.save(screening), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}/screenings")
+    public ResponseEntity<List<Screening>> getAll(@PathVariable(name="id") int movieId) {
+        Movie movie = movieRepository.findById(movieId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No movie with that ID was found.")
+        );
+
+        // maybe findAll(...) can also filter
+        return ResponseEntity.ok(screeningRepository.findAll().stream().filter(
+                s -> s.getMovieId() == movie.getId()
+        ).toList());
     }
 }
--- src/main/java/com/booleanuk/api/cinema/controller/ScreeningController.java
+++ src/main/java/com/booleanuk/api/cinema/controller/ScreeningController.java
@@ -1,6 +1,5 @@
 package com.booleanuk.api.cinema.controller;
 
-import com.booleanuk.api.cinema.model.Movie;
 import com.booleanuk.api.cinema.model.Screening;
 import com.booleanuk.api.cinema.repository.MovieRepository;
 import com.booleanuk.api.cinema.repository.ScreeningRepository;
@@ -9,11 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @RestController
-@RequestMapping("/movies/{id}/screenings")
+@RequestMapping("/screenings")
 public class ScreeningController {
     ScreeningRepository screeningRepository;
     MovieRepository movieRepository;
@@ -23,34 +19,10 @@ public class ScreeningController {
         this.movieRepository = movieRepository;
     }
 
-    @PostMapping
-    public ResponseEntity<Screening> create(@PathVariable int movieId, @RequestBody Screening screening) {
-        Movie movie = movieRepository.findById(movieId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No movie with that ID was found.")
-        );
-        screening.setMovie(movie);
-        screening.setCreatedAt(LocalDateTime.now());
-        screening.setUpdatedAt(LocalDateTime.now());
-        return new ResponseEntity<>(screeningRepository.save(screening), HttpStatus.CREATED);
-    }
-
-    // Works, but it's weird to have to add a movie id
-    @GetMapping("/{s_id}")
-    public ResponseEntity<Screening> getOne(@PathVariable(name="id") int id, @PathVariable(name="s_id") int s_id) {
-        return ResponseEntity.ok(screeningRepository.findById(s_id).orElseThrow(
+    @GetMapping("/{id}")
+    public ResponseEntity<Screening> getOne(@PathVariable(name="id") int id) {
+        return ResponseEntity.ok(screeningRepository.findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No screening with that ID was found.")
         ));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Screening>> getAll(@PathVariable(name="id") int movieId) {
-        Movie movie = movieRepository.findById(movieId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No movie with that ID was found.")
-        );
-
-        // maybe findAll(...) can also filter
-        return ResponseEntity.ok(screeningRepository.findAll().stream().filter(
-                s -> s.getMovieId() == movie.getId()
-        ).toList());
     }
 }
